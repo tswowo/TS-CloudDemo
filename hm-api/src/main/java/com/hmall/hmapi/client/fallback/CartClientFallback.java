e9432ebe4a1b732b799167419d9f026b379e235e
tswowo6 <tswossion@outlook.com>
2026-07-23 19:49:25 +0800

    fix: 为cartclient新增了fallback，现在所有client都有fallback

---
+++ hm-api/src/main/java/com/hmall/hmapi/client/fallback/CartClientFallback.java
@@ -0,0 +1,23 @@
+package com.hmall.hmapi.client.fallback;
+
+import com.hmall.common.exception.BizIllegalException;
+import com.hmall.hmapi.client.CartClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FallbackFactory;
+
+import java.util.List;
+
+
+@Slf4j
+public class CartClientFallback implements FallbackFactory<CartClient> {
+    @Override
+    public CartClient create(Throwable cause) {
+        return new CartClient() {
+            @Override
+            public void deleteCartItemByIds(List<Long> ids) {
+                log.error("远程调用CartClient#deleteCartItemByIds方法出现异常，参数：{}", ids, cause);
+                throw new BizIllegalException(cause);
+            }
+        };
+    }
+}
