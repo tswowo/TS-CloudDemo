b3b944a88ba8e0aca5e509a13f5dad0bd6dabd79
tswowo6 <tswossion@outlook.com>
2026-08-20 00:37:27 +0800

    针对面试为项目完善了一些功能 使用Redisson

--- cart-service/src/main/java/com/tscloud/cart/config/CartProperties.java
+++ cart-service/src/main/java/com/tscloud/cart/config/CartProperties.java
@@ -2,9 +2,11 @@ package com.tscloud.cart.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Data
+@RefreshScope
 @ConfigurationProperties(prefix = "ts.cart")
 @Component
 public class CartProperties {
